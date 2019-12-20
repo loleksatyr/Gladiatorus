b45d904fa44cf7ce0cb9e8ac7d0fe2b0e6e8ac2d
loleksatyr <czarnotabartek@gmail.com>
2019-12-20 17:58:23 +0100

    Update AI.cs

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -13,6 +13,7 @@ public class AI : MonoBehaviour
         float step = speed * Time.deltaTime;
         transform.position= Vector3.MoveTowards(transform.position, target.position, step);
         transform.position = new Vector3(transform.position.x,0.97f ,transform.position.z);
-    }
+        transform.LookAt(target);
+}
 }
 
