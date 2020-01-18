5cbf10a961b2c2b9c8417897c6602aeb5525d97e
Azakes <48621291+Azakes@users.noreply.github.com>
2020-01-18 19:41:59 +0100

    Test-comit
    Coś sprawdzam

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TouchPadPosition
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchPadPosition;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
