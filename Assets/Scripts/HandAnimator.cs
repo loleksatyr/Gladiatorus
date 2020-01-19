8c97891be92df0c47a51d8fa55557c9001f492b0
Azakes <48621291+Azakes@users.noreply.github.com>
2020-01-19 07:21:47 +0100

    New system of motion and hands v0.2
    Zaimportowałem nowy ruch i pracuję nad rękami

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandAnimator : MonoBehaviour
+{
+    public SteamVR_Action_Single m_GrabAction = null;
+
+    private Animator m_Animator = null;
+    private SteamVR_Behaviour_Pose m_Pose = null;
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+
+        m_GrabAction[m_Pose.inputSource].onChange -= Grab;
+
+    }
+
+    private void OnDestroy()
+    {
+        m_GrabAction[m_Pose.inputSource].onChange -= Grab;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_Animator.SetBool("Point", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_Animator.SetBool("Point", false);
+    }
+
+    private void Grab(SteamVR_Action_Single action, SteamVR_Input_Sources source, float axis, float delta)
+    {
+        m_Animator.SetFloat("GrabBlend", axis);
+    }
+
+
+
+}
